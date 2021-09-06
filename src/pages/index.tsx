6d5d7b2e63d87cb11e1bab734a36dd111d70ed10
Tiago <tscs2@cin.ufpe.br>
2021-09-06 16:18:39 -0300

    Adiciona contexto de player funcional e estilização de botões

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,9 @@ import { convertDurationToTimeString } from "../utils/convertDurationToTimeStrin
 
 import styles from './home.module.scss';
 
+import { useContext } from "react";
+import { PlayerContext } from "../contexts/PlayerContext";
+
 type Episode = {
   id: string;
   title: string;
@@ -28,6 +31,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -54,7 +59,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={ () => play(episode)}>
                   <img src="/play-green.svg" alt="Play episode"/>
                 </button>
               </li>
